f95274c4075c2cd65d017cf21cea4145547af71c
Jona <jona@Jonathans-MacBook-Pro.local>
2020-10-18 14:07:42 -0500

    inside

--- src/views/HomeView/Hero/Hero.js
+++ src/views/HomeView/Hero/Hero.js
@@ -10,9 +10,21 @@ import {
   makeStyles,
   Button
 } from '@material-ui/core';
-
+import Clouds from 'vanta/dist/vanta.clouds.min'
+import BIRDS from 'vanta/dist/vanta.birds.min'
+import TRUNK from 'vanta/dist/vanta.trunk.min'
+import * as THREE from 'three'
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.dark,
+    paddingTop: 0,
+    paddingBottom: 120,
+    [theme.breakpoints.down('md')]: {
+      paddingTop: 60,
+      paddingBottom: 60
+    }
+  },
   technologyIcon: {
     height: 40,
     margin: theme.spacing(1)
@@ -49,70 +61,105 @@ const useStyles = makeStyles((theme) => ({
 
 const Hero = ({ className, data, ...rest }) => {
   const classes = useStyles();
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const vantaRef = useRef();
+
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(BIRDS({
+        THREE,
+        el: vantaRef.current,
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        backgroundColor: 0x232c4a,
+        color1: 0x115f48,
+        color2: 0x971e6,
+        // birdSize: 1.20,
+        // wingSpan: 23.00,
+        // speedLimit: 6.00,
+        // separation: 75.00,
+        // alignment: 48.00,
+        // cohesion: 24.00
+      }))
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy()
+    }
+  }, [vantaEffect])
 
   return (
-
-    <Container maxWidth="lg">
-      {
-        data !== null &&
-        <Grid
-          container
-          spacing={3}
-        >
+    <div
+      id={'hero'}
+      className={classes.root}
+      ref={vantaRef}
+    >
+      <Container maxWidth="lg">
+        {
+          data !== null &&
           <Grid
-            item
-            xs={12}
-            md={5}
+            container
+            spacing={3}
           >
-            <Box
-              display="flex"
-              flexDirection="column"
-              justifyContent="center"
-              height="100%"
+            <Grid
+              item
+              xs={12}
+              md={5}
             >
-              <Typography
-                variant="h1"
-                className={classes.title}
-
+              <Box
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
+                height="100%"
               >
-                {data.title}
-              </Typography>
-              <Box mt={3}>
                 <Typography
-                  variant="body1"
-                  color="textSecondary"
-                  className={classes.description}
+                  variant="h1"
+                  className={classes.title}
+
                 >
-                  {data.description}
+                  {data.title}
                 </Typography>
-              </Box>
-              <Box mt={3} className={classes.buttons}>
-                <Button variant="contained" color="primary" target="_blank" href={data.github} startIcon={<GitHubIcon />}>
-                  Github
+                <Box mt={3}>
+                  <Typography
+                    variant="body1"
+                    color="textSecondary"
+                    className={classes.description}
+                  >
+                    {data.description}
+                  </Typography>
+                </Box>
+                <Box mt={3} className={classes.buttons}>
+                  <Button variant="contained" color="primary" target="_blank" href={data.github} startIcon={<GitHubIcon />}>
+                    Github
                 </Button>
-                <Button variant="contained" color="primary" target="_blank" href={data.website} startIcon={<BusinessIcon />}>
-                  Company
+                  <Button variant="contained" color="primary" target="_blank" href={data.website} startIcon={<BusinessIcon />}>
+                    Company
                 </Button>
+                </Box>
               </Box>
-            </Box>
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              md={7}
+            >
+              <Box position="relative">
+                <div className={classes.image}>
+                  <img
+                    alt="Presentation"
+                    src="https://res.cloudinary.com/dnv0qwkrk/image/upload/v1602952758/wordpress_Jonathan/Pngtree_technology_network_data_globe_4869521_tcupiz.png"
+                  />
+                </div>
+              </Box>
+            </Grid>
           </Grid>
-          <Grid
-            item
-            xs={12}
-            md={7}
-          >
-            <Box position="relative">
-              <div className={classes.image}>
-                <img
-                  alt="Presentation"
-                  src="https://res.cloudinary.com/dnv0qwkrk/image/upload/v1602952758/wordpress_Jonathan/Pngtree_technology_network_data_globe_4869521_tcupiz.png"
-                />
-              </div>
-            </Box>
-          </Grid>
-        </Grid>
-      }
-    </Container>
+        }
+      </Container>
+    </div>
   );
 };
 
--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -11,10 +11,7 @@ import Portfolio from './Portfolio'
 import Footer from './Footer';
 import clsx from 'clsx';
 import { getPersonalDataAction } from 'src/redux/actions/personalDataAction'
-import Clouds from 'vanta/dist/vanta.clouds.min'
-import BIRDS from 'vanta/dist/vanta.birds.min'
-import TRUNK from 'vanta/dist/vanta.trunk.min'
-import * as THREE from 'three'
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -25,43 +22,15 @@ const useStyles = makeStyles((theme) => ({
           paddingTop: 60,
           paddingBottom: 60
         }
-      },}));
+      },
+    }));
 
 const HomeView = () => {
     const classes = useStyles();
     const dispatch = useDispatch();
     const resumeData = useSelector(state => state.personalDataState.personalData);
     const [name, setName] = useState('Jonathan');
-    const [vantaEffect, setVantaEffect] = useState(0);
-    const vantaRef = useRef();
 
-    useEffect(() => {
-        if (!vantaEffect) {
-          setVantaEffect(BIRDS({
-            THREE,
-            el: vantaRef.current,
-            mouseControls: true,
-            touchControls: true,
-            gyroControls: false,
-            minHeight: 200.00,
-            minWidth: 200.00,
-            scale: 1.00,
-            scaleMobile: 1.00,
-            backgroundColor: 0x232c4a,
-            color1: 0x115f48,
-            color2: 0x971e6,
-            // birdSize: 1.20,
-            // wingSpan: 23.00,
-            // speedLimit: 6.00,
-            // separation: 75.00,
-            // alignment: 48.00,
-            // cohesion: 24.00
-          }))
-        }
-        return () => {
-          if (vantaEffect) vantaEffect.destroy()
-        }
-      }, [vantaEffect])
 
     useEffect(() => {
         ReactGA.initialize('UA-110570651-1');
@@ -76,15 +45,7 @@ const HomeView = () => {
         <Page
             title="Home"
         >
-            {resumeData.main &&
-                <div
-                    id={'hero'}
-                    className={classes.root}
-                    ref={vantaRef}
-                >
-                    <Hero data={resumeData.main}/>
-                </div>
-            }
+            {resumeData.main &&<Hero data={resumeData.main}/>}
             {resumeData.main !== undefined && <About data={resumeData.main} />}
             {resumeData.testimonials !== undefined && <Testimonials data={resumeData.testimonials} />}
             {resumeData.resume !== undefined && <Resume data={resumeData.resume} />}
