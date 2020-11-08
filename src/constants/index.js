a580e70e9e4ff94e88b4205a2fa91e7e0781a76e
Jona <jona@Jonathans-MacBook-Pro.local>
2020-11-07 20:12:20 -0500

    Add service workers

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,5 @@
-export const APP_VERSION = '3.1.0';
-
 export const ENABLE_REDUX_DEV_TOOLS = true;
-
+export const BASE_URL = process.env.REACT_APP_ENDPOINT;
 export const THEMES = {
   LIGHT: 'LIGHT',
   DARK: 'DARK',
